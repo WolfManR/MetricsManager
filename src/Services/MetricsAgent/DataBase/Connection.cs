b31db0904b260498e58f64d8ec762e29ab8c7686
Tarxos <ftarxos@gmail.com>
2021-10-13 05:22:49 +0300

    create connection builder

---
+++ src/Services/MetricsAgent/DataBase/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SQLite;
+
+namespace MetricsAgent.DataBase;
+
+public class Connection : ICreateConnectionStage, IConnectionConfigurationStage
+{
+    private string _connectionString;
+
+    private Connection()
+    {
+
+    }
+
+    public static IConnectionConfigurationStage Create()
+    {
+        return new Connection();
+    }
+
+    public SQLiteConnection CreateSQLiteConnection() => new SQLiteConnection(_connectionString);
+    public ICreateConnectionStage WithConnectionString(string connectionString)
+    {
+        _connectionString = connectionString;
+        return this;
+    }
+}
+
+public interface IConnectionConfigurationStage
+{
+    public ICreateConnectionStage WithConnectionString(string connectionString);
+}
+
+public interface ICreateConnectionStage
+{
+    public SQLiteConnection CreateSQLiteConnection();
+}
