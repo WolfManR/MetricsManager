c48b39a18fdf240e3d56d22498708ccb55450248
Tarxos <ftarxos@gmail.com>
2021-10-13 06:29:22 +0300

    add quarts jobs services

---
+++ src/Services/MetricsAgent/Jobs/JobFactory.cs
@@ -0,0 +1,23 @@
+﻿using Quartz.Spi;
+using Quartz;
+
+namespace MetricsAgent.Jobs;
+
+public class JobFactory : IJobFactory
+{
+    private readonly IServiceProvider _provider;
+
+    public JobFactory(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+    {
+        return _provider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
+    }
+
+    public void ReturnJob(IJob job)
+    {
+    }
+}
---
+++ src/Services/MetricsAgent/Jobs/JobRegistrationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace MetricsAgent.Jobs;
+
+public static class JobRegistrationExtensions
+{
+    public static IServiceCollection AddJob<TJob>(this IServiceCollection services, string cronExpression) where TJob : class, IJob
+    {
+        return services
+            .AddSingleton<TJob>()
+            .AddSingleton(new JobSchedule(typeof(TJob), cronExpression));
+    }
+}
