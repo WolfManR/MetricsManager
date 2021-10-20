a7eebdffcd2c74417bf72f2561a761efba58b827
Tarxos <ftarxos@gmail.com>
2021-10-21 00:15:08 +0300

    add quartz services

---
+++ src/Services/MetricsManagerAPI/QuartzService/QuartzHostedService.cs
@@ -0,0 +1,64 @@
+﻿using Quartz;
+using Quartz.Spi;
+
+namespace MetricsManagerAPI.QuartzService;
+
+public class QuartzHostedService : IHostedService
+{
+    private readonly ISchedulerFactory _schedulerFactory;
+    private readonly IJobFactory _jobFactory;
+    private readonly IEnumerable<JobSchedule> _schedules;
+
+    public QuartzHostedService(ISchedulerFactory schedulerFactory, IJobFactory jobFactory, IEnumerable<JobSchedule> schedules)
+    {
+        _schedulerFactory = schedulerFactory;
+        _jobFactory = jobFactory;
+        _schedules = schedules;
+    }
+
+    public IScheduler Scheduler { get; set; }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
+        Scheduler.JobFactory = _jobFactory;
+
+        foreach (var schedule in _schedules)
+        {
+            var job = CreateJobDetail(schedule);
+            var trigger = CreateTrigger(schedule);
+
+            await Scheduler.ScheduleJob(job, trigger, cancellationToken);
+        }
+
+        await Scheduler.Start(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if (Scheduler is not null)
+        {
+            await Scheduler.Shutdown(cancellationToken);
+        }
+    }
+
+    private static IJobDetail CreateJobDetail(JobSchedule schedule)
+    {
+        var jobType = schedule.JobType;
+        return JobBuilder
+            .Create(jobType)
+            .WithIdentity(jobType.FullName ?? string.Empty)
+            .WithDescription(jobType.Name)
+            .Build();
+    }
+
+    private static ITrigger CreateTrigger(JobSchedule schedule)
+    {
+        return TriggerBuilder
+            .Create()
+            .WithIdentity($"{schedule.JobType.FullName}.trigger")
+            .WithCronSchedule(schedule.CronExpression)
+            .WithDescription(schedule.CronExpression)
+            .Build();
+    }
+}
